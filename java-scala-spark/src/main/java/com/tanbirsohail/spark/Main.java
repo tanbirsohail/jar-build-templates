7f8c3fedf19b230631a8d23af60e102e26dbb534
Tanbir Sohail <tanbirsohail@users.noreply.github.com>
2020-12-08 13:31:24 +0530

    Updated Main class to print spark version and config

--- java-scala-spark/src/main/java/com/tanbirsohail/spark/Main.java
+++ java-scala-spark/src/main/java/com/tanbirsohail/spark/Main.java
@@ -1,12 +1,23 @@
 package com.tanbirsohail.spark;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SparkSession;
+import scala.Function1;
+import scala.Tuple2;
+import scala.collection.immutable.Map;
+
+import java.util.Arrays;
 
 public class Main {
-    private static final SparkConf sparkConf = new SparkConf().setAppName("CsvToParquetJava").setMaster("local");
+    private static final SparkConf sparkConf = new SparkConf().setAppName("Main").setMaster("local");
     private static final SparkSession spark = SparkSession.builder().config(sparkConf).getOrCreate();
     public static void main(String[] args) {
         System.out.println("Spark Version is : " + spark.version());
+
+        System.out.println("Printing all spark configurations : ");
+        JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
+        Tuple2<String, String>[] allConf = jsc.getConf().getAll();
+        Arrays.stream(allConf).iterator().forEachRemaining( (t) -> System.out.println(t._1 + " : "+ t._2));
     }
 }
